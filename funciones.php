b88717b1db8d83012facc9e0e51add1eb3c3a7e2
NicolasMRomero <nicolas.m.romero@gmail.com>
2018-04-07 19:21:35 -0300

    dos últimas funciones para registrar

--- funciones.php
+++ funciones.php
@@ -135,4 +135,26 @@ function loguear($usuario) {
 }
 //function estaLogueado() {}
 //function traerPorId($id){}
+
+// == FUNCTION - traerId ==
+/*
+  - Recibe un parámetro -> $id:
+  - Devuelve el usuario si encuentra a alguno con ese ID
+  - Devuelve false si no hay usuarios con ese ID
+*/
+function traerPorId($id){
+  // me traigo todos los usuarios
+  $todos = traerTodos();
+
+  // Recorro el array de todos los usuarios
+  foreach ($todos as $usuario) {
+    if ($id == $usuario['id']) {
+      return $usuario;
+    }
+  }
+
+  return false;
+}
+
+
  ?>
